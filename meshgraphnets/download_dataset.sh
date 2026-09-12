9f7559446af920723e6b93a9546d177ac26fb6d1
Jackson Scott <168689027+jxnscott@users.noreply.github.com>
2026-09-12 12:07:12 -0500

    Feature/output dir quotes (#1)
    * Quote MeshGraphNets dataset output directory
    
    * altering readme to remove progress bars

--- meshgraphnets/download_dataset.sh
+++ meshgraphnets/download_dataset.sh
@@ -25,7 +25,7 @@ OUTPUT_DIR="${2}/${DATASET_NAME}"
 
 BASE_URL="https://storage.googleapis.com/dm-meshgraphnets/${DATASET_NAME}/"
 
-mkdir -p ${OUTPUT_DIR}
+mkdir -p "${OUTPUT_DIR}"
 for file in meta.json train.tfrecord valid.tfrecord test.tfrecord
 do
 wget -O "${OUTPUT_DIR}/${file}" "${BASE_URL}${file}"
